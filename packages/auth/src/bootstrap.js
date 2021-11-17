c3a346f126f4e3d84a2f60442b8a9683884d7929
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-11-17 11:15:53 +0100

    initial commit

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
+
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory || createMemoryHistory({ initialEntries: [initialPath] });
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDom.render(<App history={history} onSignIn={onSignIn} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    }
+  };
+};
+
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_auth-dev-root");
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+export { mount };
---
+++ packages/container/src/App.js
@@ -0,0 +1,49 @@
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import { Switch, Route, Redirect, Router } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import { createBrowserHistory } from "history";
+import Header from "./components/Header";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co"
+});
+
+const history = createBrowserHistory();
+
+export default () => {
+  const [isSignedIn, setIsSigned] = useState(false);
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    } else {
+      history.push();
+    }
+  }, [isSignedIn]);
+  return (
+    <Router history={history}>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => setIsSigned(false)}
+          />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSigned(true)} />
+              </Route>
+              <Route path="/dashboard">
+                <DashboardLazy />
+              </Route>
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
+        </div>
+      </StylesProvider>
+    </Router>
+  );
+};
