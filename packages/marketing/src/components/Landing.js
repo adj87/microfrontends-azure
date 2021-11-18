b5f01eaba40e56f828d3f979788923730febed57
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-11-18 16:58:55 +0100

    landing and sign in page changed

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -87,9 +87,7 @@ export default function Album() {
               color="textSecondary"
               paragraph
             >
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don&apos;t simply skip over it entirely.
+              This is a microfrontends environment.
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
