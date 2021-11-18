16ef88d7ad7c24bf844d12f7045302ba29b143c4
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-11-18 14:34:43 +0100

    try-32

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,8 +5,8 @@ import { createBrowserHistory } from "history";
 import Header from "./components/Header";
 
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
-// const AuthLazy = lazy(() => import("./components/AuthApp"));
-// const DashboardLazy = lazy(() => import("./components/DashboardApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co"
